8c9993eb86bac00f92f6f4aed1f21f74809e7efa
Varik Matevosyan <varikmatevosyan@gmail.com>
2024-04-30 14:47:43 -0700

    Fix jump loop for delete_old_backups in lantern timeline

--- spec/prog/lantern/lantern_timeline_nexus_spec.rb
+++ spec/prog/lantern/lantern_timeline_nexus_spec.rb
@@ -94,7 +94,12 @@ RSpec.describe Prog::Lantern::LanternTimelineNexus do
     it "hops to delete_old_backups if needs cleanup" do
       expect(lantern_server.timeline).to receive(:need_backup?).and_return(false)
       expect(lantern_server.timeline).to receive(:need_cleanup?).and_return(true)
-      expect { nx.wait }.to hop("delete_old_backups")
+      expect(timeline).to receive(:leader).and_return(lantern_server)
+      retain_after = (Time.new - (24*60*60*Config.backup_retention_days)).strftime('%Y-%m-%dT%H:%M:%S.%LZ')
+      expect(lantern_server.vm.sshable).to receive(:cmd).with(a_string_matching("common/bin/daemonizer 'docker compose -f /var/lib/lantern/docker-compos.yaml exec -T -u root postgresql bash -c"))
+      expect(lantern_server.timeline).to receive(:backups).and_return([{last_modified: Time.now - 1 * 24 * 60 * 60}])
+      expect(lantern_server.timeline).to receive(:leader).and_return(lantern_server)
+      expect { nx.wait }.to nap(20 * 60)
     end
 
     it "puts missing backup error for 2 days" do
@@ -188,14 +193,6 @@ RSpec.describe Prog::Lantern::LanternTimelineNexus do
       expect(lantern_server.timeline).to receive(:destroy)
       expect(lantern_server.timeline).to receive(:children).and_return([])
       expect { nx.destroy }.to exit({"msg" => "lantern timeline is deleted"})
-    end
-  end
-
-  describe "#delete_old_backups" do
-    it "deletes old backups and hops to wait" do
-      expect(timeline).to receive(:leader).and_return(lantern_server)
-      expect(lantern_server.vm.sshable).to receive(:cmd).with("common/bin/daemonizer 'sudo lantern/bin/delete_old_backups' delete_old_backups", stdin: JSON.generate({ retain_after: (Time.new - (24*60*60*Config.backup_retention_days)).strftime('%Y-%m-%dT%H:%M:%S.%LZ') } ))
-      expect { nx.delete_old_backups }.to hop("wait")
     end
   end
 end
