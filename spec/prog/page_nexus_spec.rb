9e138531dcdc2b1f24bd420a4c27e08b8c14d72b
Varik Matevosyan <varikmatevosyan@gmail.com>
2024-04-29 19:46:34 -0700

    Add severity to page, add manual backup ability, add list backups route to api

--- spec/prog/page_nexus_spec.rb
+++ spec/prog/page_nexus_spec.rb
@@ -34,14 +34,14 @@ RSpec.describe Prog::PageNexus do
     it "does not create duplicate" do
       expect(Page).to receive(:from_tag_parts).and_return(pg)
       expect(Page).not_to receive(:create_with_id)
-      described_class.assemble_with_logs("test", [], {})
+      described_class.assemble_with_logs("test", [], {}, "error")
     end
 
     it "creates a new page with logs" do
       expect(Page).to receive(:from_tag_parts).and_return(nil)
-      expect(Page).to receive(:create_with_id).with(summary: "test", details: {"related_resources" => [], "logs" => {"stdout" => "test logs"}}, tag: "").and_return(pg)
+      expect(Page).to receive(:create_with_id).with(summary: "test", details: {"related_resources" => [], "logs" => {"stdout" => "test logs"}}, tag: "", severity: "error").and_return(pg)
       expect(Strand).to receive(:create)
-      described_class.assemble_with_logs("test", [], {"stdout" => "test logs"})
+      described_class.assemble_with_logs("test", [], {"stdout" => "test logs"}, "error")
     end
   end
 end
