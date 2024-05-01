0d7299daa9beb779f17a9157836226e844b50837
Varik Matevosyan <varikmatevosyan@gmail.com>
2024-04-30 20:58:20 -0700

    Ignore timeout error when waiting for gcp operation

--- routes/api/project/lantern.rb
+++ routes/api/project/lantern.rb
@@ -17,5 +17,51 @@ class CloverApi
         count: result[:count]
       }
     end
+
+    r.post true do
+      Authorization.authorize(@current_user.id, "Postgres:create", @project.id)
+      parsed_size = Validation.validate_lantern_size(r.params["size"])
+      Validation.validate_name(r.params["name"])
+      Validation.validate_org_id(r.params["org_id"])
+      restore_target = nil
+
+      if r.params["parent_id"].nil?
+        Validation.validate_version(r.params["lantern_version"], "lantern_version")
+        Validation.validate_version(r.params["extras_version"], "extras_version")
+        Validation.validate_version(r.params["minor_version"], "minor_version")
+      else
+        restore_target = r.params["restore_target"].nil? ? Time.new : Time.new(r.params["restore_target"])
+      end
+
+      domain = r.params["domain"]
+
+      if domain.nil? && r.params["subdomain"]
+        domain = "#{r.params["subdomain"]}.#{Config.lantern_top_domain}"
+      end
+
+      st = Prog::Lantern::LanternResourceNexus.assemble(
+        project_id: @project.id,
+        location: r.params["location"],
+        name: r.params["name"],
+        org_id: r.params["org_id"].to_i,
+        target_vm_size: parsed_size.vm_size,
+        target_storage_size_gib: r.params["storage_size_gib"] || parsed_size.storage_size_gib,
+        lantern_version: r.params["lantern_version"],
+        extras_version: r.params["extras_version"],
+        minor_version: r.params["minor_version"],
+        domain: domain,
+        db_name: r.params["db_name"],
+        db_user: r.params["db_user"],
+        db_user_password: r.params["db_user_password"],
+        app_env: r.params["app_env"] || Config.rack_env,
+        repl_password: r.params["repl_password"],
+        enable_debug: r.params["enable_debug"],
+        superuser_password: r.params["postgres_password"],
+        parent_id: r.params["parent_id"],
+        restore_target: restore_target
+      )
+      pg = LanternResource[st.id]
+      serialize(pg, :detailed)
+    end
   end
 end
