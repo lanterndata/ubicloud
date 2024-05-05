6adda6551b85026b8bf0edaf0d6e88e5d965f9b3
Varik Matevosyan <varikmatevosyan@gmail.com>
2024-05-04 20:59:57 -0700

    add more misc ops, force fetch timeline before generating walg creds

--- misc/misc_operations.rb
+++ misc/misc_operations.rb
@@ -31,6 +31,26 @@ def update_rhizome(sshable, target_folder, user)
 end
 
 class MiscOperations
+  def self.update_collation_on_all_databases
+    # https://postgresql.verite.pro/blog/2018/08/27/glibc-upgrade.html
+    # We didn't need to update rebuild indexes this time as we didn't have any indexes with collation coming from libc
+    resources = LanternResource.all
+    resources.each do |resource|
+      update_collation resource
+    end
+  end
+
+  def self.update_collation(resource)
+    all_dbs = resource.representative_server.run_query("SELECT datname from pg_database WHERE datname != 'template0'").split("\n")
+    all_dbs.each do |db|
+      resource.representative_server.run_query("ALTER DATABASE #{db} REFRESH COLLATION VERSION")
+    end
+  end
+
+  def self.active_queries(resource_name)
+    LanternResource[name: resource_name].representative_server.run_query("SELECT pid, query FROM pg_stat_activity")
+  end
+
   def self.update_rhizome_from_local(vm, target_folder: "lantern", user: "lantern")
     update_rhizome(vm.sshable, target_folder, user)
   end
@@ -53,5 +73,63 @@ class MiscOperations
 
   def self.task_logs(resource_name, task_name)
     LanternResource[name: resource_name].representative_server.vm.sshable.cmd("common/bin/daemonizer --logs #{task_name}")
+  end
+
+  def self.query_on_db(vm, db_name, query)
+    vm.sshable.cmd("sudo docker compose -f /var/lib/lantern/docker-compose.yaml exec -T postgresql psql -U postgres -t --csv #{db_name}", stdin: query)
+  end
+
+  def self.reindex_all_concurrently(resource_name, disable_indexes: false)
+    serv = LanternResource[name: resource_name].representative_server
+    all_dbs = serv.vm.sshable.cmd("sudo docker compose -f /var/lib/lantern/docker-compose.yaml exec postgresql psql -U postgres -P \"footer=off\" -c 'SELECT datname from pg_database' | tail -n +3 | grep -v 'template0' | grep -v 'template1'").strip.split("\n")
+    command_list = []
+    all_dbs.each do |db|
+      db_name = db.strip
+      indexes = MiscOperations.query_on_db(serv.vm, db_name, "SELECT i.relname::text
+                              FROM pg_class t
+                              JOIN pg_index ix ON t.oid = ix.indrelid
+                              JOIN pg_class i ON i.oid = ix.indexrelid
+                              JOIN pg_am a ON i.relam = a.oid
+                              JOIN pg_namespace n ON n.oid = i.relnamespace
+                              WHERE a.amname = 'lantern_hnsw';").strip.split("\n")
+
+      if !indexes.empty?
+        queries = []
+        indexes.each {
+          queries.push("REINDEX INDEX CONCURRENTLY #{_1};")
+          if disable_indexes
+            queries.push("UPDATE pg_index SET indisvalid = false, indisready = false WHERE indexrelid = quote_ident('#{_1}')::regclass::oid;")
+          end
+        }
+
+        queries.map do |query|
+          command_list.push("sudo docker compose -f /var/lib/lantern/docker-compose.yaml exec -T postgresql psql -U postgres #{db_name} -c \"#{query}\"")
+        end
+      end
+    end
+    command = command_list.join("\n")
+
+    serv.vm.sshable.cmd("cp /dev/stdin /tmp/reindex_all_concurrently.sh && chmod +x /tmp/reindex_all_concurrently.sh", stdin: command)
+    serv.vm.sshable.cmd("common/bin/daemonizer 'sudo /tmp/reindex_all_concurrently.sh' reindex_all_concurrently")
+
+    task_logs resource_name, "reindex_all_concurrently"
+  end
+
+  def self.get_all_lantern_indexes(resource_name)
+    serv = LanternResource[name: resource_name].representative_server
+    all_dbs = serv.vm.sshable.cmd("sudo docker compose -f /var/lib/lantern/docker-compose.yaml exec postgresql psql -U postgres -P \"footer=off\" -c 'SELECT datname from pg_database' | tail -n +3 | grep -v 'template0' | grep -v 'template1'").strip.split("\n")
+    all_indexes = []
+    all_dbs.each do |db|
+      db_name = db.strip
+      indexes = MiscOperations.query_on_db(serv.vm, db_name, "SELECT i.relname::text
+                              FROM pg_class t
+                              JOIN pg_index ix ON t.oid = ix.indrelid
+                              JOIN pg_class i ON i.oid = ix.indexrelid
+                              JOIN pg_am a ON i.relam = a.oid
+                              JOIN pg_namespace n ON n.oid = i.relnamespace
+                              WHERE a.amname = 'lantern_hnsw';").strip.split("\n")
+      all_indexes += indexes
+    end
+    all_indexes
   end
 end
