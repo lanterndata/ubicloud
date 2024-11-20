ca695a5354a07768fe3929bf17becc192a88bcdb
Varik Matevosyan <varikmatevosyan@gmail.com>
2024-11-20 12:02:22 +0400

    watch logical replication after enabling, to refresh subscription each 20s

--- model/lantern/lantern_resource.rb
+++ model/lantern/lantern_resource.rb
@@ -159,7 +159,6 @@ SQL
        EXECUTE format('SET ROLE %I', NEW.session_user);
        EXECUTE NEW.ddl_command;
        RESET ROLE;
-       ALTER SUBSCRIPTION sub_#{ubid} REFRESH PUBLICATION;
        RETURN NEW;
    END;
    $$ LANGUAGE plpgsql;
